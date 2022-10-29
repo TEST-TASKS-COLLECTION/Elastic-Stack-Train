daede77872d37fea4f4401dc62534b97fafc1d75
mikeyy2 <adhikari.shrish1@gmail.com>
2022-10-29 18:27:09 +0545

    Added a little structure to the code

--- project/elastic_app/__init__.py
+++ project/elastic_app/__init__.py
@@ -5,9 +5,9 @@ from elasticsearch import Elasticsearch
 
 from flask import Flask, jsonify, request
 
+from .helpers import get_doc, check_doc_exists, create_data, delete_document
 
 # ELASTIC_PASSWORD = "pw"
-
 # username = "username"
 # password = "password"
 # host_ip = "host_ip"
@@ -32,8 +32,6 @@ client = Elasticsearch(
 # Successful response!
 # print(client.info())
 
-
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -48,81 +46,6 @@ def ping_pong():
             "message": "pong!"
         }
     )
-
-def get_index(client, index, id):
-    pass
-    client.get()
-
-def create_data(client, index, id, data):
-    """
-    Create a document with the provided id in the index
-    Args:
-        client (_type_): client (ElasticSearch object) 
-        index (_type_): index for the document where it shall belong
-        id (_type_): id of the document
-        data (_type_): data that the document should have
-    send_data_as:
-        client.create("test-index-2", id="420", body={
-        "title": "man",
-        "feed": "back lol"
-    # })
-    """
-    print("----------------CREATING DOCUMENT--------------------")
-    return client.create(index=index, id=id, body=data)
-
-def delete_document(client, index, id):
-    """
-    Delete a document with the provided id in the index
-    Args:
-        client (_type_): client (ElasticSearch object) 
-        index (_type_): index for the document where it shall belong
-        id (_type_): id of the document
-    send_data_as:
-        client.create("test-index-2", id="420", body={
-        "title": "man",
-        "feed": "back lol"
-    # })
-    """
-    print("----------------DELETING DOCUMENT--------------------")
-    res = client.delete(index=index, id=id)
-    if res['result']:
-        print(res['result'])
-    return res
-
-def check_doc_exists(client, index, id):
-    """
-    Check if a document with the provided id in the index exists
-    Args:
-        client (_type_): client (ElasticSearch object) 
-        index (_type_): index for the document where it shall belong
-        id (_type_): id of the document
-    send_data_as:
-        client.create("test-index-2", id="420", body={
-        "title": "man",
-        "feed": "back lol"
-    # })
-    """
-    print("----------------CHECKING IF DOCUMENT EXISTS--------------------")
-    res = client.exists(index=index, id=id)
-    return res
-
-
-def get_doc(client, index, id):
-    """
-    Returns a document with the provided id in the index
-    Args:
-        client (_type_): client (ElasticSearch object) 
-        index (_type_): index for the document where it shall belong
-        id (_type_): id of the document
-    send_data_as:
-        client.create("test-index-2", id="420", body={
-        "title": "man",
-        "feed": "back lol"
-    # })
-    """
-    print("----------------GETTING A DOCUMENT--------------------")
-    res = client.get(index=index, id=id)
-    return res
 
 @app.route("/status", methods=["GET"],)
 def api_status():
